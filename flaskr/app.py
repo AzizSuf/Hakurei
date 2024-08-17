cddb4b996c9f924c0b0a72153c0e7b84c9167264
Aziz Sufyanov <aziz-sufyanov@mail.ru>
2024-08-17 22:50:46 +0300

    База

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
